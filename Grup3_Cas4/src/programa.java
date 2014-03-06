125dac7cffc65dfed9c95147dfbe92b5080dd425
Manel <Manel@Manel-PC>
2014-03-06 09:14:22 +0100

    1r versio

--- Grup3_Cas4/src/programa.java
+++ Grup3_Cas4/src/programa.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.Scanner;
 
-class programa {
+class programa{
 
 	public static int factorial (int numero){
 		 
@@ -18,51 +18,70 @@ class programa {
     	
     	System.out.print("Benvinguts al programa de Grup1!!! ");
        	System.out.print("\n----------------------------------\n\n");
-        
-       	System.out.print("Tria una opció. 1- Factorial o 2- Potencies:  ");
+        do{
+        	System.out.println("\t MENU");
+        	               	        	           	
+        	System.out.print("Tria una opció. Factorial o Potencies:  ");
+        	Scanner opt=new Scanner(System.in);
+        	int opcio = opt.nextInt();
+        	switch(opcio) {
 
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		        case 1:
+		        	Scanner escriu_numero=new Scanner(System.in);
+		    		int numero = 0;
+		    		
+		    		System.out.println("\nÉs l'opció factorial");		
+		    		System.out.print("\n");
+		    		
+		    		while(numero != '*'){
+		    	    System.out.print("Introduex un numero: ");
+		    		
+		    	    numero = escriu_numero.nextInt();
+		    			
+		    	    while(numero < 0){
+		    	    	if(numero < 0){
+			   	     		System.out.println("Error");
+			   	     		System.out.print("Escriu un número enter i positiu: ");
+			   	     	}
+			   	    	numero = escriu_numero.nextInt();
+		    	    }
+		   	    	System.out.println(factorial(numero));         		 
+		   
+		    		}
+		    	break;
 
-        String str = br.readLine();
-        int opcio = Integer.parseInt(str);
-
-        if (opcio == 1){
-        	Scanner escriu_numero=new Scanner(System.in);
-    		int numero;
-    		
-    		System.out.println("\nés l'opció factorial");		
-    		System.out.print("\n");
-    		
-    	    System.out.print("Escriu un número enter i positiu: ");
-    	    
-    	    numero = escriu_numero.nextInt();
-    	   
-    	    while(numero < 0){
-    	    	if(numero < 0){
-	   	     		System.out.println("Error");
-	   	     	}
-	   	    	numero = escriu_numero.nextInt();
-    	    }
-   	    	System.out.println(factorial(numero));         		 
-   
-        }
-        
-        else if (opcio == 2){
-        	Scanner in = new Scanner(System.in);
-            int numero, exponent;
-            int potencia;
+		        case 2:
+		        	Scanner in = new Scanner(System.in);
+		            int numero1=0, exponent;
+		            int potencia;
+		           
+		            System.out.println("\nÉs l'opció potencia ");
+		            System.out.print("\n");
+		            
+		            while(numero1 != '*'){
+			            
+		            	System.out.print("Introduex un numero: ");
+		            	numero1 = in.nextInt();
+			           
+			            System.out.print("Introduex un exponent: ");
+			            exponent = in.nextInt();
+			
+			            potencia= (int) Math.pow(numero1,exponent);
+			            System.out.println("Valor de potencia: " + potencia);
+			            System.out.print("\n");
+		            }
+		        break;
+       
+		        default:
+		        	System.out.println("Error" );
+		        break;
             
-            System.out.println("\nés l'opció potencia ");
-        	System.out.print("\n");
 
-            System.out.print("Introduex un numero: ");
-            numero = in.nextInt();
             
-            System.out.print("Introduex un exponent: ");
-            exponent = in.nextInt();
-
-            potencia=(int) Math.pow(numero,exponent);
-            System.out.println("Valor de potencia: " + potencia);
-        }
+        	}
+        }while(true);
     }
 }
+      
+        
+       	
