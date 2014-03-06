55c29a6cdf3b11aef613ab535b3aeeb6a343f328
Manel <Manel@Manel-PC>
2014-03-06 10:13:30 +0100

    versio 5 (Introduce Parameter Object)

---
+++ Grup3_Cas4/src/ParametresCalculfactorial.java
@@ -0,0 +1,8 @@
+
+public class ParametresCalculfactorial {
+	public int numero;
+
+	public ParametresCalculfactorial(int numero) {
+		this.numero = numero;
+	}
+}
---
+++ Grup3_Cas4/src/versio5.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.Scanner;
+
+class versio5 implements interficiefactorialpotencia{
+
+	private static int numerodelusuari;
+
+	public static int calculfactorial (ParametresCalculfactorial parameterObject){
+		 
+		if(numerodemanat(parameterObject.numero)==0)
+		return 1;
+		
+		else{
+			int resultat = numerodemanat(parameterObject.numero) * calculfactorial(new ParametresCalculfactorial(numerodemanat(parameterObject.numero)-1));
+			return numerodemanat(resultat);
+		}
+	}
+
+	public static int numerodemanat(int numero) {
+		return numero;
+	}
+	
+    public static void main (String[] args )throws IOException{
+    	
+    	System.out.print("Benvinguts al programa de Grup1!!! ");
+       	System.out.print("\n----------------------------------\n\n");
+        do{
+        	System.out.println("\t MENU");
+        	               	        	           	
+        	System.out.print("Tria una opció. Factorial o Potencies:  ");
+        	Scanner opt=new Scanner(System.in);
+        	int opcio = opt.nextInt();
+        	switch(numerodemanat(opcio)) {
+
+		        case 1:
+		        	Scanner escriu_numero=new Scanner(System.in);
+				numerodelusuari = 0;
+		    		
+		    		System.out.println("\nÉs l'opció factorial");		
+		    		System.out.print("\n");
+		    		
+		    		while(numerodemanat(numerodelusuari) != '*'){
+		    	    System.out.print("Introduex un numero: ");
+		    		
+		    	    numerodelusuari = escriu_numero.nextInt();
+		    			
+		    	    while(numerodemanat(numerodelusuari) < 0){
+		    	    	if(numerodemanat(numerodelusuari) < 0){
+			   	     		System.out.println("Error");
+			   	     		System.out.print("Escriu un número enter i positiu: ");
+			   	     	}
+			   	    	numerodelusuari = escriu_numero.nextInt();
+		    	    }
+		   	    	System.out.println(calculfactorial(new ParametresCalculfactorial(numerodemanat(numerodelusuari))));         		 
+		   
+		    		}
+		    	break;
+
+		        case 2:
+		        	Scanner in = new Scanner(System.in);
+		            int numero1=0, exponent;
+		            int potencia;
+		           
+		            System.out.println("\nÉs l'opció potencia ");
+		            System.out.print("\n");
+		            
+		            while(numerodemanat(numero1) != '*'){
+			            
+		            	System.out.print("Introduex un numero: ");
+		            	numero1 = in.nextInt();
+			           
+			            System.out.print("Introduex un exponent: ");
+			            exponent = in.nextInt();
+			
+			            potencia= (int) Math.pow(numerodemanat(numero1),numerodemanat(exponent));
+			            System.out.println("Valor de potencia: " + numerodemanat(potencia));
+			            System.out.print("\n");
+		            }
+		        break;
+       
+		        default:
+		        	System.out.println("Error" );
+		        break;
+            
+
+            
+        	}
+        }while(true);
+    }
+}
+      
+        
+       	
